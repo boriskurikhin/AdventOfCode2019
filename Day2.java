3e200a360d6cc96a5e637caf95880666f16c29dd
Boris Skurikhin <boriskurikhin@gmail.com>
2019-12-05 16:37:06 -0500

    fix <= 99 condition

--- Day2.java
+++ Day2.java
@@ -8,8 +8,8 @@ public class Day4 {
 	public static void main(String[] args) throws Exception {
 		inputCopy = Arrays.stream(r.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
 		
-		for (int x = 0; x < 99; x++) {
-			for (int y = 0; y < 99; y++) {
+		for (int x = 0; x <= 99; x++) {
+			for (int y = 0; y <= 99; y++) {
 				
 				input = Arrays.copyOf(inputCopy, inputCopy.length);
 				
