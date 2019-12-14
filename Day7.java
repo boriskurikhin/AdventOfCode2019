1c68198c40ad1488797bf67d4e405a0f62876f7e
Boris Skurikhin <boriskurikhin@gmail.com>
2019-12-13 21:15:41 -0500

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -1,127 +1,254 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
-class Computer {
-	long input[], INPUT_VALUE_1, output;
-	int i; /* Program Pointer */
-	boolean isHalted, firstInput;
-	List<Long> outputArr;
-	Computer send;
-	
-	public Computer(long input[], long INPUT_VALUE_1) {
-		this.input = input;
-		this.INPUT_VALUE_1 = INPUT_VALUE_1;
-		this.isHalted = false;
-		this.i = 0; /* IP -> instruction pointer */
-		this.outputArr = new ArrayList<Long>();
-		this.firstInput = false;
-	}
-	
-	public void setup(Computer send) {
-		this.send = send;
-	}
-	
-	public long run(long INPUT_VALUE_2) {
-		int length = input.length;
-		outer:
-		while (i < length) {
-			/* Get this stuff sorted out */
-			int opCode = (int) input[i] % 100;
-			long aMode = input[i] / 100 % 10;
-			long bMode = input[i] / 1000 % 10;
-			//int cMode = input[i] / 10000 % 10;
-			/* We don't care about cMode at all */
-			
-			switch (opCode) {			
-				case 1:
-					input[(int)input[i + 3]] = (aMode == 1 ? input[i + 1] : input[(int)input[i + 1]]) + (bMode == 1 ? input[i + 2] : input[(int)input[i + 2]]);
-					i += 4;
-					break;
-				case 2:
-					input[(int)input[i + 3]] = (aMode == 1 ? input[i + 1] : input[(int)input[i + 1]]) * (bMode == 1 ? input[i + 2] : input[(int)input[i + 2]]);
-					i += 4;
-					break;
-				case 3: 
-					if (!firstInput) {
-						input[(int)input[i+1]] = INPUT_VALUE_1; /* I think this what it means by provide input */
-						firstInput = true;
-					} else {
-						input[(int)input[i+1]] = INPUT_VALUE_2;
-					}
-					i += 2;
-					break;
-				case 4: 
-					output = input[(int)input[i+1]];
-					/* We need to send it to the next amplifier */
-					i += 2;
-					break outer;
-				case 5:
-					if ((aMode == 1) ? (input[i + 1] != 0) : (input[(int)input[i + 1]] != 0)) 
-						i = (int) (bMode == 1 ? input[i + 2] : input[(int)input[i + 2]]);
-					else i += 3;
-					break;
-				case 6:
-					if ((aMode == 1) ? (input[i + 1] == 0) : (input[(int)input[i + 1]] == 0)) 
-						i = (int) (bMode == 1 ? input[i + 2] : input[(int)input[i + 2]]);
-					else i += 3;
-					break;
-				case 7:
-					if ((aMode == 1 ? input[i + 1] : input[(int)input[i + 1]]) < (bMode == 1 ? input[i + 2] : input[(int)input[i + 2]]))
-						input[(int)input[i + 3]] = 1;
-					else input[(int)input[i + 3]] = 0;
-					i += 4;
-					break;
-				case 8:
-					if ((aMode == 1 ? input[i + 1] : input[(int)input[i + 1]]) == (bMode == 1 ? input[i + 2] : input[(int)input[i + 2]])) 
-						input[(int)input[i + 3]] = 1;
-					else input[(int)input[i + 3]] = 0;
-					i += 4;
-					break;
-				case 99:
-					this.isHalted = true;
-					break;
-			}
-		}
-		return output;
-	}
-}
+/*
+ * Okay, so my code is not entirely original. I looked at the following solution
+ * ----> https://github.com/akaritakai/AdventOfCode2019/blob/master/src/main/java/net/akaritakai/aoc2019/Puzzle07.java
+ * 
+ * This guy is really talented, and did an amazing job multithreading and using concurrent hashmaps.
+ * The way he set up his code is amazing. I tried my best understanding his solution and implementing my own
+ * on top of it. It seems like he was using Lombok to write his code which I don't have. 
+ * 
+ * So this is vanilla Java. But very functional. 
+ * 
+ */
 
 
 public class Day7 {
+	public static String programInstructions;
+	public static final long SETTINGS[] = {5L,6L,7L,8L,9L};
+	public static long result = Long.MIN_VALUE;
 
-	static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-	static long[] input; 
-	static Computer[] amplifiers;
-	static long bestValue = Long.MIN_VALUE;
 	
-
 	public static void main(String[] args) throws Exception {
-		input = Arrays.stream(r.readLine().split(",")).mapToLong(Long::parseLong).toArray();
-		trySettings(new ArrayList<Integer>());
-		System.out.println(bestValue);
+		System.out.println(new Day7().getAnswer());
 	}
-		
-	//Works for part 1
-	public static void trySettings (List<Integer> setting) {
-		if (setting.size() == 5) {
-			Computer A = new Computer(input, setting.get(0));
-			long result = A.run(0);
-			for (int i = 1; i < 5; i++) 
-				result = new Computer(input, setting.get(i)).run(result);
-			bestValue = Math.max(bestValue, result);
+	
+	public long getAnswer() throws Exception {
+		BufferedReader r = new BufferedReader(new FileReader("src/input.txt"));
+		programInstructions = r.readLine();
+		r.close();
+		computePhaseSettings(new ArrayList<Long>());
+		return result;
+	}
+	
+	private void computePhaseSettings (List<Long> phaseSettings) throws Exception {
+		if (phaseSettings.size() == SETTINGS.length) {
+			result = Math.max(result, runAmplifiers(phaseSettings));
+			return;
 		}
 		
-		/* Combinatorics */
-		for (int i = 0; i < 5; i++) {
-			if (!setting.contains(i)) {
-				setting.add(i);
-				trySettings(setting);
-				setting.remove(setting.indexOf(i));
+		for (long setting : SETTINGS) {
+			if (!phaseSettings.contains(setting)) {
+				phaseSettings.add(setting);
+				computePhaseSettings(phaseSettings);
+				phaseSettings.remove(phaseSettings.indexOf(setting));
 			}
+		}
+	}
+	
+	
+	private long runAmplifiers(List<Long> phaseSettings) throws Exception {
+		System.out.println(phaseSettings);
+		List<LinkedBlockingQueue<Long>> ioStreams = Stream.generate((Supplier<LinkedBlockingQueue<Long>>) LinkedBlockingQueue::new)
+		        .limit(phaseSettings.size())
+		        .collect(Collectors.toList());
+		
+		List<Thread> amplifiers = IntStream.range(0, phaseSettings.size())
+				.mapToObj(i -> {
+					System.out.println(i);
+					Long phaseSetting = phaseSettings.get(i);
+					LinkedBlockingQueue<Long> currentAmplifier = ioStreams.get(i);
+					LinkedBlockingQueue<Long> nextAmplifier = ioStreams.get((i + 1) % phaseSettings.size());
+					try {
+						return runSingleAmplifier(phaseSetting, currentAmplifier, nextAmplifier);
+					} catch (InterruptedException e) { 
+						return null; 
+					}
+				})
+				.collect(Collectors.toList());
+		ioStreams.get(0).add(0L); //We send that initial 0 to the first amplifier
+		//wait for all threads to die
+		amplifiers.forEach(t -> {
+			try {
+				t.join();
+			} catch (InterruptedException e) {}
+		});
+		//Whatever was sent as the input to the first amplifier is the result of the last amplifier
+		return ioStreams.get(0).remove();
+	}
+	
+	/* Returns a thread which in turns runs an instance of an amplifier. This thread takes 2 streams as parameter */
+	private Thread runSingleAmplifier (long phaseSetting, BlockingQueue<Long> inputStream, BlockingQueue<Long> outputStream) throws InterruptedException {
+		inputStream.put(phaseSetting);
+		Thread thread = new Thread(() -> {
+			Computer intComputer = new Computer(
+					programInstructions,
+					() -> {
+						try {
+							//Passes function as argument
+							return inputStream.take();
+						} catch (InterruptedException e) { return null; }
+					},
+					t -> {
+						try {
+							outputStream.put(t);
+						} catch (InterruptedException e) { }
+					});
+			
+			intComputer.run();
+		});
+		thread.start();
+		return thread;
+	}
+}
+
+class Computer {
+	long ip = 0; /* Program Pointer */
+	private Map<Long, Long> memory;
+	private Supplier<Long> _input; //stream of inputs
+	private Consumer<Long> _output; //stream of outputs
+	private enum OpCode {
+		HALT 	(99),
+		ADD 	(1),
+		MULTIPLY(2),
+		INPUT 	(3),
+		OUTPUT	(4),
+		JUMP_T	(5),
+		JUMP_F 	(6),
+		LESS	(7),
+		EQ		(6);
+		
+		private final long op_code;
+		
+		OpCode (long code) {
+			op_code = code;
+		}
+		
+		/* Returns the enum value of an OP_CODE */
+		private static OpCode codeName (long code) {
+			return Arrays.stream(OpCode.values())
+					.filter(opcode -> opcode.op_code == code)
+					.findAny()
+					.orElseThrow(() -> new UnsupportedOperationException("Unknown Code: " + code));
+		}
+		
+	}
+	private enum Mode {
+		POSITION (0),
+		IMMEDIATE(1);
+		
+		private final long mode;
+		
+		Mode (long _mode) {
+			mode = _mode;
+		}
+		
+		/* Returns the enum value of an OP_CODE */
+		private static Mode modeName (long _mode) {
+			return Arrays.stream(Mode.values())
+					.filter(m -> m.mode == _mode)
+					.findAny()
+					.orElseThrow(() -> new UnsupportedOperationException("Unknown Mode: " + _mode));
+		}
+	}
+	
+	public Computer(String instructions, Supplier<Long> supplier, Consumer<Long> consumer) {
+		this.memory = allocateMemory(instructions);
+		this._input = supplier;
+		this._output = consumer;
+	}
+	
+	/* Retrieves current opCode as an ENUM */
+	private OpCode opCode () {
+		return OpCode.codeName(memory.get(ip) % 100L);
+	}
+	
+	private Mode mode (int parameterNumber) {
+		long operation = memory.getOrDefault(ip, 0L); //0 by default
+		return Mode.modeName( operation / (long) Math.pow(10, 1 + parameterNumber) % 10);
+		
+	}
+	
+	private Parameter p (int parameterNumber) {
+		return new Parameter(parameterNumber);
+	}
+	
+	private Map<Long, Long> allocateMemory (String instructions) {
+		Map<Long, Long> memoryMap = new ConcurrentHashMap<Long, Long>();
+		Iterator<Long> data = Arrays.stream(instructions.split(",")).map(Long::parseLong).iterator();
+		for (long i = 0; data.hasNext(); i++)
+			memoryMap.put(i, data.next());
+		return memoryMap;
+	}
+	
+	public synchronized void run() {
+		while (true) {
+			switch (opCode()) {
+				case ADD:
+					memory.put(p(3).address, p(1).value + p(2).value);
+					ip += 4;
+					break;
+				case MULTIPLY:
+					memory.put(p(3).address, p(1).value * p(2).value);
+					ip += 4;
+					break;
+				case INPUT:
+					memory.put(p(1).address, _input.get());
+					ip += 2;
+					break;
+				case OUTPUT:
+					_output.accept(p(1).value);
+					ip += 2;
+					break;
+				case JUMP_T:
+					ip = p(1).value != 0 ? p(2).value : ip + 3;
+					break;
+				case JUMP_F:
+					ip = p(1).value == 0 ? p(2).value : ip + 3;
+					break;
+				case LESS:
+					memory.put(p(3).address, p(1).value < p(2).value ? 1L : 0L);
+					ip += 4;
+					break;
+				case EQ:
+					memory.put(p(3).address, p(1).value == p(2).value ? 1L : 0L);
+					ip += 4;
+					break;
+				case HALT:
+					return;
+			}
+		}
+	}
+	
+	private final class Parameter { 
+		public long address;
+		public long value;
+		public Parameter (int parameterNumber) {
+			switch (mode(parameterNumber)) {
+				case POSITION:
+					address = memory.getOrDefault(ip + parameterNumber, 0L);
+					break;
+				case IMMEDIATE:
+					address = ip + parameterNumber;
+					break;
+				default: throw new UnsupportedOperationException("Unknown Mode for: " + parameterNumber);
+			}
+			value = memory.getOrDefault(address, 0L);
 		}
 	}
 }
